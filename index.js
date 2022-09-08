ffe7c8aceb063be440c6e30f9f97e4cea5ade0f4
Archan26 <archanparmar99@gmail.com>
2022-09-08 17:04:53 +0530

    update validation

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 let flattenObject = {}
-getFlattenObject = (object, name) => {
+
+let getFlattenObject = (object, name) => {
+    if (typeof object === 'object') {
+        if (name === undefined) {
+            return "Please pass the second parameter - any string"
+        }
+        return convertObject(object, name)
+    } else {
+        return "Please pass the valid object"
+    }
+}
+
+convertObject = (object, name) => {
     for (const key in object) {
         if (typeof object[key] === 'object') {
-            getFlattenObject(object[key], name + "_" + key);
+            convertObject(object[key], name + "_" + key);
         } else {
             flattenObject[name + "_" + key] = object[key]
         }
     }
     return flattenObject;
 };
-
 
 module.exports = getFlattenObject;
